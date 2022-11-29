3d1944dfbb0ed9c1158f5f9055e50ccbbe26242a
Jonathan Gendre <47138032+jonsoninlaw@users.noreply.github.com>
2022-11-29 19:09:06 +0100

    Update Admin2.vue

--- src/views/Admin2.vue
+++ src/views/Admin2.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="admin">
-    <div>
-      <label>
-        Nom
-        <input v-model="name" />
-      </label>
+  <div class="flex flex-col items-center w-full xmas">
+    <div class="flex flex-col items-stretch justify-center mb-8">
+      <input
+        @keyup.enter="addUser"
+        type="text"
+        class="flex items-center w-full px-4 mb-2 leading-normal text-gray-700 border rounded-lg shadow appearance-none h-11 focus:outline-none focus:shadow-outline"
+        v-model="userName"
+      /><button
+        @click="addUser"
+        class="flex items-center w-full px-6 text-white bg-green-800 rounded-lg h-11 hover:bg-green-600 focus:outline-none"
+      >
+        Ajouter un participant
+      </button>
     </div>
-    <button @click="addUser()">Ajouter un participant</button>
   </div>
 </template>
 
@@ -14,11 +20,11 @@
 import { addDoc, collection, getFirestore } from "@firebase/firestore";
 import { ref } from "vue";
 
-const name = ref("");
+const userName = ref("");
 
 async function addUser() {
   const data = {
-    name: name.value,
+    name: userName.value,
     isDrawn: false,
     hasDrawn: false,
   };
@@ -27,8 +33,8 @@ async function addUser() {
 
   try {
     await addDoc(collection(db, "user"), data);
-    console.log(`${name.value} a été ajouté`);
-    name.value = "";
+    console.log(`${userName.value} a été ajouté`);
+    userName.value = "";
   } catch (error) {
     console.log(error);
   }
